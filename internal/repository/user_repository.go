1a65ebc203a651e04fa313217a639bee5f96e72a
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2026-04-11 12:48:31 +0300

    Add user entity, repository, migration

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"github-release-notifier/internal/domain"
+)
+
+type UserStore struct {
+	db *sql.DB
+}
+
+func NewUserStore(db *sql.DB) *UserStore { return &UserStore{db} }
+
+func (s *UserStore) CreateIfNotExists(ctx context.Context, email string) (domain.User, error) {
+	query := `
+		insert into users (email)
+		values ($1)
+		on conflict (email) do update set email = $1
+		returning id, email, created_at, updated_at;
+	`
+
+	var result domain.User
+
+	err := s.db.QueryRowContext(ctx, query, email).Scan(
+		&result.ID,
+		&result.Email,
+		&result.CreatedAt,
+		&result.UpdatedAt,
+	)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return result, nil
+}
